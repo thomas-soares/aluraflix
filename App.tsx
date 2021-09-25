b138f3a47a0725e5934420f4eed53f4a30310c2e
Thomas Soares <thomas__soares@hotmail.com>
2021-09-24 22:58:04 -0300

    Adicionando input

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World, NLW Together</Text>
+      <TextInput style={styles.input} onChangeText={setText} />
       <StatusBar style="auto" />
     </View>
   );
