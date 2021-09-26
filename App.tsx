88559447f8084a64d49b781ae0cf43a02691b609
Thomas Soares <thomas__soares@hotmail.com>
2021-09-25 22:54:38 -0300

    Hook do estado do input

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+
   return (
     <View style={styles.container}>
       <Text>Hello World, NLW Together</Text>
