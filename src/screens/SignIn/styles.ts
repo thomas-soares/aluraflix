4b0eb3df05bc32bd8e9f89d20d4d7ddfadf4d1ab
Thomas Soares <thomas__soares@hotmail.com>
2021-09-29 22:49:06 -0300

    Estilo do container

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -2,4 +2,11 @@ import { StyleSheet } from "react-native";
 
 import { theme } from "../../global/styles/theme";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colors.background,
+  },
+});
