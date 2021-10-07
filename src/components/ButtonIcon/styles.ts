79a5a8f96d209e5b5047c71d2d3643cd9f37ad2f
Thomas Soares <thomas__soares@hotmail.com>
2021-10-06 23:14:54 -0300

    Tamanho do container do componente

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -1,4 +1,9 @@
 import { Stylesheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
-export const styles = Stylesheet.create({});
+export const styles = Stylesheet.create({
+  container: {
+    width: "100%",
+    height: 56,
+  },
+});
