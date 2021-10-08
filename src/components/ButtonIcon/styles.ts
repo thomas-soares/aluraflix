d6809302afff6c19e9ea2bbe0c9950be59053f41
Thomas Soares <thomas__soares@hotmail.com>
2021-10-07 23:21:42 -0300

    Aplicando border radius no botao

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -6,6 +6,7 @@ export const styles = Stylesheet.create({
     width: "100%",
     height: 56,
     backgroundColor: theme.colors.primary,
+    borderRadius: 8,
     flexDirection: "row",
     alignItems: "center",
   },
