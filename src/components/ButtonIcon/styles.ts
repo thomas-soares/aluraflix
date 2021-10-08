e5827a02a5ecacddd0b8c2f0a91aa7e639e22726
Thomas Soares <thomas__soares@hotmail.com>
2021-10-07 23:21:12 -0300

    Posicionamento do botao

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -6,5 +6,7 @@ export const styles = Stylesheet.create({
     width: "100%",
     height: 56,
     backgroundColor: theme.colors.primary,
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
