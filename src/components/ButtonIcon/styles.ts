4ce10490ab77c5972b5e1173a90ac152ebcc7513
Thomas Soares <thomas__soares@hotmail.com>
2021-10-10 23:15:17 -0300

    Ajuste do container do botao

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -6,9 +6,6 @@ export const styles = Stylesheet.create({
     width: "100%",
     height: 56,
     backgroundColor: theme.colors.primary,
-    borderRadius: 8,
-    flexDirection: "row",
-    alignItems: "center",
   },
   title: {
     flex: 1,
