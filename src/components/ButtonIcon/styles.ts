75eb1f018f48e3686011a55f0d1f6d613d6c62c0
Thomas Soares <thomas__soares@hotmail.com>
2021-10-11 23:32:38 -0300

    Removendo estilo do container do componente

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -1,10 +1,4 @@
 import { Stylesheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
-export const styles = Stylesheet.create({
-  container: {
-    width: "100%",
-    height: 56,
-    backgroundColor: theme.colors.primary,
-  },
-});
+export const styles = Stylesheet.create({});
