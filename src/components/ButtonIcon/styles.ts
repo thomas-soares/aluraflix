c357bec5f624a43cf9a8abe2e0bb87a9ff03db80
Thomas Soares <thomas__soares@hotmail.com>
2021-10-12 23:22:49 -0300

    estilo container do componente

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -1,4 +1,13 @@
 import { Stylesheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
-export const styles = Stylesheet.create({});
+export const styles = Stylesheet.create({
+  container: {
+    width: "100%",
+    height: 56,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
