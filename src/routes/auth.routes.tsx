9ce06005f7a0cc4463ab0f340e2e721ad064f8d2
Thomas Soares <thomas__soares@hotmail.com>
2021-10-16 23:34:16 -0300

    Criando componente de navegaçao

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
+const { Navigator, Screen } = createStackNavigator;
+
 export function AuthRoutes() {
   return <Navigator></Navigator>;
 }
