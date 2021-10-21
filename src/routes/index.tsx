5f40fc368adcfc49fbd2ea1208040bcb8f71ebb4
Thomas Soares <thomas__soares@hotmail.com>
2021-10-20 23:33:35 -0300

    Exportando funçao das rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,3 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AuthRoutes } from "./auth.routes";
+
+export function Routes() {
+  return ()
+}
