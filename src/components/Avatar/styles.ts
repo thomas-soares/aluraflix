c04c846ea7c3c6a4e2ce1baecc8cceb13d4b3227
Thomas Soares <thomas__soares@hotmail.com>
2021-10-22 23:33:51 -0300

    Tamanho do container

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -1,3 +1,8 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    width: 48,
+    height: 48,
+  },
+});
