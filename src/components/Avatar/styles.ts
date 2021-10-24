186e2e257fee840708ec142640ae90d1ce36b543
Thomas Soares <thomas__soares@hotmail.com>
2021-10-23 23:24:39 -0300

    Estilo do container

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -4,8 +4,10 @@ export const styles = StyleSheet.create({
   container: {
     width: 48,
     height: 48,
+    borderRadius: 8,
     alignItems: "center",
     justifyContent: "center",
+    marginRight: 22,
   },
   avatar: {
     width: 46,
