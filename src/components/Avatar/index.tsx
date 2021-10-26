5f5882aa30ebef11e30c9e8f04a3570e03586cea
Thomas Soares <thomas__soares@hotmail.com>
2021-10-25 23:17:34 -0300

    Importando estilos do componente

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -2,4 +2,6 @@ import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { Image } from "react-native";
 
+import { styles } from "./styles";
+
 export function Avatar() {}
