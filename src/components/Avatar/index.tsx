a1aeb75f7028c64dbd14385910118a70f7d5350c
Thomas Soares <thomas__soares@hotmail.com>
2021-10-25 23:21:45 -0300

    Importando tema do projeto

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,5 +3,6 @@ import { LinearGradient } from "expo-linear-gradient";
 import { Image } from "react-native";
 
 import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
 
 export function Avatar() {}
