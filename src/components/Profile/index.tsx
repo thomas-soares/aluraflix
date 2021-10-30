befd2c0169267cc44344614d29ab5a36192305dd
Thomas Soares <thomas__soares@hotmail.com>
2021-10-29 23:02:59 -0300

    Mensagem footer

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -13,6 +13,7 @@ export function Profile() {
           <Text style={{styles.greeting}}>Olá</Text>
           <Text style={{styles.username}}>Thomas</Text>
         </View>
+          <Text style={{styles.message}}>Hoje é dia de vitoria</Text>
       </View>
     </View>
   );
