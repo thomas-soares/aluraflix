45033b673c0a83bdedc53606a53b8da945afe611
Thomas Soares <thomas__soares@hotmail.com>
2021-10-31 23:16:35 -0300

    Estilo do container do profile

--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -1,4 +1,8 @@
 import { StyleSheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
-export const styles = Stylesheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+});
