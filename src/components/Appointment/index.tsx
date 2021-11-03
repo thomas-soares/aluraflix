c23a336ab7d711a56e2ca5ecef0df85d747c9a77
Thomas Soares <thomas__soares@hotmail.com>
2021-11-02 23:52:58 -0300

    Props do componente

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { RectButton } from "react-native-gesture-handler";
 import { View } from "react-native";
 
+export type AppointmentProps = {
+  id: string;
+};
+
 export function Appointment() {
   return <View style={styles.container}></View>;
 }
