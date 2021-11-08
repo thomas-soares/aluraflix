8707912ca8257e6a8555bcf492d1cc5ab3b4c4cc
Thomas Soares <thomas__soares@hotmail.com>
2021-11-07 22:58:20 -0300

    Para ver se o botao foi clicado

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { View } from "react-native";
+import { styles } from "../Avatar/styles";
 
 export function Category() {
   return (
-    <View>
+    <View style={checked ? styles.checked : styles.check}>
       <Icon width={48} height={48} />
     </View>
   );
