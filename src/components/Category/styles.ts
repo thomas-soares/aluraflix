764cc6828bcc3b595da17240231fda8a6c01417c
Thomas Soares <thomas__soares@hotmail.com>
2021-11-10 23:12:03 -0300

    cor do titulo de category

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -2,4 +2,8 @@ import { StyleSheet } from "react-native";
 
 import { theme } from "../../global/styles/theme";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  title: {
+    color: theme.colors.heading,
+  },
+});
