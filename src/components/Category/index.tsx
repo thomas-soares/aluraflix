9b736dec89d9ff3bbd6ff4411265cbd76ba21a7e
Thomas Soares <thomas__soares@hotmail.com>
2021-11-16 23:04:15 -0300

    View no category

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -17,6 +17,7 @@ export function Category({ title, icon, checked = false, ...rest }: Props) {
         <View style={checked ? styles.checked : styles.check}>
           <Icon width={48} height={48} />
         </View>
+        <View style={styles.content}></View>
         <Image source={{ uri: UrlImage }} style={styles.avatar} />
         <Text style={styles.title}>{text}</Text>
       </linearGradient>
