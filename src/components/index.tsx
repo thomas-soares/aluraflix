3376c6759d73fdd0883b83fa88f057017faf5cdf
Thomas Soares <thomas__soares@hotmail.com>
2021-11-17 23:29:47 -0300

    Create index.tsx

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+import { styles } from "./styles";
+
+export function CategorySelect() {
+  return <ScrollView horizontal style={styles.container}></ScrollView>;
+}
