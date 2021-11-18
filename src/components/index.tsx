3faadac839b2895b6f88b5136f03182a467b58f3
Thomas Soares <thomas__soares@hotmail.com>
2021-11-17 23:32:03 -0300

    Update index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,6 +3,8 @@ import { ScrollView } from "react-native";
 
 import { styles } from "./styles";
 
+import { Category } from '../Category';
+
 export function CategorySelect() {
   return <ScrollView horizontal style={styles.container}></ScrollView>;
 }
