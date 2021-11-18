3fc3e95bcd77e14170dde90175cbf97ef7cd8f74
Thomas Soares <thomas__soares@hotmail.com>
2021-11-17 23:30:57 -0300

    Create styles.ts

---
+++ src/components/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  title: {
+    color: theme.colors.heading,
+    fontSize: 15,
+  },
+  check: {
+    width: 10,
+  },
+});
