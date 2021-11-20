b1d465dbf4d34928d23714fc654ac4fc2243e181
Thomas Soares <thomas__soares@hotmail.com>
2021-11-19 22:39:21 -0300

    Update styles.ts

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -3,8 +3,5 @@ import { StyleSheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-  title: {
-    color: theme.colors.heading,
-  
-  },
+ 
 });
