4d54440c255f19a2b2dcd9a85a09b1aefbec5c0d
Thomas Soares <thomas__soares@hotmail.com>
2021-11-20 23:11:54 -0300

    Update styles.ts

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,7 +1,3 @@
 import { StyleSheet } from "react-native";
 
-import { theme } from "../../global/styles/theme";
-
-export const styles = StyleSheet.create({
- 
-});
+export const styles = StyleSheet.create({});
