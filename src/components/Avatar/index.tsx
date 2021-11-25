4c736e717b2bbba0176491ba79c9ea16c3a34335
Thomas Soares <thomas__soares@hotmail.com>
2021-11-24 23:19:43 -0300

    Update index.tsx

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -5,9 +5,6 @@ import { Image } from "react-native";
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
 
-type Props = {
-};
-
 export function Avatar({ urlImage }: Props) {
   const { secondary80, secondary100 } = theme.colors;
 
