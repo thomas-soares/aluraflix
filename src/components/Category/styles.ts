dc1ee5beeee146a547b110b72d657bbae5384462
Thomas Soares <thomas__soares@hotmail.com>
2021-11-29 22:21:43 -0300

    Update styles.ts

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -3,8 +3,6 @@ import { StyleSheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-  title: {
-  },
   check: {
     width: 10,
   },
