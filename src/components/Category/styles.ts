a11f26c2e3ee8c4db5eed3270ed5a4124d47061d
Thomas Soares <thomas__soares@hotmail.com>
2021-11-29 22:21:19 -0300

    Update styles.ts

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -5,7 +5,6 @@ import { theme } from "../../global/styles/theme";
 export const styles = StyleSheet.create({
   title: {
     color: theme.colors.heading,
-    fontSize: 15,
   },
   check: {
     width: 10,
