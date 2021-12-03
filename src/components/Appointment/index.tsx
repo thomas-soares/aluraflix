55b87a1e43d999f38e732b595b7e0695ef2bec2a
Thomas Soares <thomas__soares@hotmail.com>
2021-12-02 22:18:48 -0300

    Update index.tsx

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -3,7 +3,5 @@ import { RectButton } from "react-native-gesture-handler";
 import { View } from "react-native";
 
 export function Appointment({ data, ...rest }: Props) {
-  return (
-    
-  );
+
 }
